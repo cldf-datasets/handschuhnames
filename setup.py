5ca12b37e29074291f464be832ffab93df50b51e
xrotwang <xrotwang@googlemail.com>
2020-10-27 15:28:06 +0100

    added skeleton and raw data

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_handschuhnames',
+    py_modules=['cldfbench_handschuhnames'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'handschuhnames=cldfbench_handschuhnames:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
