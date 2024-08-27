edf89cc5bcf8625f9f6d2f5bfde490f171ba0321
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:07:20 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_handschuhnames',
-    py_modules=['cldfbench_handschuhnames'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'handschuhnames=cldfbench_handschuhnames:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
